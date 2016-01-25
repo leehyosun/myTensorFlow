9ab8e4aa2007a1b5847e53468eb9dbb4bd2d5b38
leehyosun <hyosun.lee@HLBRD10-NA101LB.LGE.NET>
2016-01-25 18:55:02 +0900

    add RefCounted
    RefCounted

---
+++ src/tensorflow/core/lib/core/refcount.h
@@ -0,0 +1,17 @@
+/*
+ * refcount.h
+ *
+ *  Created on: 2016. 1. 25.
+ *      Author: hyosun.lee
+ */
+
+#ifndef TENSORFLOW_CORE_LIB_CORE_REFCOUNT_H_
+#define TENSORFLOW_CORE_LIB_CORE_REFCOUNT_H_
+
+class RefCounted {
+
+};
+
+
+
+#endif /* TENSORFLOW_CORE_LIB_CORE_REFCOUNT_H_ */
