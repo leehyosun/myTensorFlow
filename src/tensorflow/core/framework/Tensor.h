8eb8727ad6ed4c81ad6c419a6ab332ee76b9eb73
leehyosun <hyosun.lee@HLBRD10-NA101LB.LGE.NET>
2016-02-01 15:49:17 +0900

    0201.3.49
    0201.3.49

---
+++ src/tensorflow/core/framework/Tensor.h
@@ -0,0 +1,18 @@
+/*
+ * Tensor.h
+ *
+ *  Created on: 2016. 1. 26.
+ *      Author: hyosun.lee
+ */
+
+/*
+ * tensorflow.core.framework 는 tensor의 기본 프레임웍
+ */
+#ifndef TENSORFLOW_CORE_FRAMEWORK_TENSOR_H_
+#define TENSORFLOW_CORE_FRAMEWORK_TENSOR_H_
+
+
+
+
+
+#endif /* TENSORFLOW_CORE_FRAMEWORK_TENSOR_H_ */
