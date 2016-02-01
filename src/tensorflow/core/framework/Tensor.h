57cf0af8e789c4ffa530dc85e834a8bdb3b48f0e
leehyosun <hyosun.lee@HLBRD10-NA101LB.LGE.NET>
2016-02-01 16:56:37 +0900

    0201.4.56
    0201.4.56

--- src/tensorflow/core/framework/Tensor.h
+++ src/tensorflow/core/framework/Tensor.h
@@ -12,7 +12,22 @@
 #define TENSORFLOW_CORE_FRAMEWORK_TENSOR_H_
 
 
+namespace tensorflow {
+
+// n 차원의 배열을 가짐
+class Tensor {
+
+private:
+	DataType type_;
+	TensorShape shape_;
+	TensorBuffer* buf_;
+
+	// 디폴트 텐서, 1차원, 0 요소의 float 텐서
+	Tensor();
 
 
+};
+
+}
 
 #endif /* TENSORFLOW_CORE_FRAMEWORK_TENSOR_H_ */
